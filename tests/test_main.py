f4461a5fd026594bc0340f82398e98893b50c2d2
lang_mm <maximilian.langheinrich@dlr.dlr>
2024-12-19 14:58:55 +0100

    Implemented unittest for move() method of Board class

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,13 @@
+
 def test_hello_world():
     assert True
+
+def test_move():
+    initial_board_status = 'o.xox..x.'
+    best_move_result = 'o.xox.ox.'
+    # Initialize board to a certain test state
+    test_board = Board('x', initial_board_status)
+    # Call the move function
+    test_board.move()
+    # Check best result
+    assert(initial_board_status == best_move_result)
