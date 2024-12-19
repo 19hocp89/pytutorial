e0ae56abb0ea23ea5f207668f2b78631592ad6be
Cevallos, Gabriel Alejandro <Gabriel.Cevallos@dlr.de>
2024-12-19 15:09:42 +0100

    first Board test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,17 @@
 def test_hello_world():
     assert True
+
+
+def test_Board_class_integrity():
+    import Board
+
+    assert True
+
+
+def test_empty_Board_initialize():
+    import Board
+
+    board = Board("o", "ooo ooo ooo")
+    assert board.rows[0] == [0, 0, 0]
+    assert board.rows[1] == [0, 0, 0]
+    assert board.rows[2] == [0, 0, 0]
