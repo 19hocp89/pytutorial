98aed792dba863d4ca039e9c2a7a849a4644525a
Philipp Hochstaffl <di35voq@hpdar03c04s06.cos.lrz.de>
2024-12-19 15:35:20 +0100

    Add comments to show test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,8 @@ def test_hello_world():
     assert True
 
 def test_show(input):
+    # Check type
     assert isinstance(input, 'something')
+    # Check size
     if input.size == 9:
         show_board(input)
